4ce47fa5fd47fcbc838edb1bc2b399f43e592c04
shockiu <galeanocarlosd@gmail.com>
2026-03-01 12:57:59 -0400

    feat: added new condig for prisma in TS ans JS

--- src/shared/infrastructure/database.ts
+++ src/shared/infrastructure/database.ts
@@ -1,5 +1,5 @@
 // src/shared/infrastructure/database.ts
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
