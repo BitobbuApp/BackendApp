90c7a9775a01de1983f758618575d399986fcbca
Carlos Galeano <59290590+shockiu@users.noreply.github.com>
2026-03-23 22:15:32 -0400

    Revert "feat: add paginated listing for CompanyOffers"

--- src/modules/companyOffers/domain/errors/companyOffer.errors.ts
+++
@@ -1,8 +0,0 @@
-// src/modules/companyOffers/domain/errors/companyOffer.errors.ts
-import { ApplicationError } from "../../../../shared/domain/error";
-
-export class CompanyOfferNotFoundError extends ApplicationError {
-    constructor(id: string) {
-        super(404, `Company Offer with ID ${id} not found`);
-    }
-}
